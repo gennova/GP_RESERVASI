eb39c1fc1242b51de6e464c5ea1f150027af7103
Tendy Developer <serendipity.10they@gmail.com>
2018-02-28 13:53:26 +0700

    home done

---
+++ src/com/shelvy/kamar/Kamar.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shelvy.kamar;
+
+/**
+ *
+ * @author Lab Komputer
+ */
+public class Kamar {
+
+    private int id_kamar;
+    private String id_kamar_str;
+    private String nama_kamar;
+    private int luas_kamar;
+    private String fasilitas;
+
+    public Kamar() {
+
+    }
+
+    public int getId_kamar() {
+        return id_kamar;
+    }
+
+    public void setId_kamar(int id_kamar) {
+        this.id_kamar = id_kamar;
+    }
+
+    public String getId_kamar_str() {
+        return id_kamar_str;
+    }
+
+    public void setId_kamar_str(String id_kamar_str) {
+        this.id_kamar_str = id_kamar_str;
+    }
+
+    public String getNama_kamar() {
+        return nama_kamar;
+    }
+
+    public void setNama_kamar(String nama_kamar) {
+        this.nama_kamar = nama_kamar;
+    }
+
+    public int getLuas_kamar() {
+        return luas_kamar;
+    }
+
+    public void setLuas_kamar(int luas_kamar) {
+        this.luas_kamar = luas_kamar;
+    }
+
+    public String getFasilitas() {
+        return fasilitas;
+    }
+
+    public void setFasilitas(String fasilitas) {
+        this.fasilitas = fasilitas;
+    }
+
+}
---
+++ src/com/shelvy/kategori/KategoriKamar.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shelvy.kategori;
+
+import java.awt.List;
+
+/**
+ *
+ * @author Lab Komputer
+ */
+public class KategoriKamar {
+    private int id;
+    private String nama_kategori;
+    
+}
